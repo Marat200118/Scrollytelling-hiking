c4ed65b074e50fe44b0dda05c5d84c6ed27e600a
Marat200118 <marat.samigullin1@gmail.com>
2023-11-19 17:14:33 +0100

    setup commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const init = () => {
+  
+  gsap.registerPlugin(ScrollTrigger);
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".square",
+      markers: { fontSize: "25px", fontWeight: "bold" },
+      start: "top 80%",
+      end: "top 30%",
+      scrub: 1,
+    },
+  });
+  tl.to(".square", {
+    x: 400,
+    duration: 3,
+  });
+  tl.to(".square", {
+    y: 200,
+    duration: 2,
+  });
+  tl.to(".square", {
+    x: 200,
+    duration: 2,
+  });
+};
+
+init();
