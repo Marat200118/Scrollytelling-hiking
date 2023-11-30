171e8a773dd3b3a56148998e740935ce37556572
Marat200118 <marat.samigullin1@gmail.com>
2023-11-30 14:36:44 +0100

    horizontal scroll fixed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
-  animateSection(".introduction");
-  animateSection(".hobby");
-  animateSection(".answer");
-  animateSection(".history");
-  animateSection(".types-locations");
-  animateSection(".future-adventures");
-  animateSection(".facts");
+  // animateSection(".introduction");
+  // animateSection(".hobby");
+  // animateSection(".answer");
+  // animateSection(".history");
+  // animateSection(".types-locations");
+  // animateSection(".future-adventures");
+  // animateSection(".facts");
 
-  document.querySelectorAll(".spacer").forEach(animateSpacer);
+  // document.querySelectorAll(".spacer").forEach(animateSpacer);
   animateVariants();
   animateHistory();
 };
@@ -42,11 +42,11 @@ const animateSection = (section) => {
   gsap.to(section, {
     scrollTrigger: {
       trigger: section,
-      start: "top top", // Start the animation when the top of the trigger hits the top of the viewport
-      end: "bottom top", // End the animation when the bottom of the trigger hits the top of the viewport
-      pin: true, // Pin the section in place while it's in view
-      scrub: true, // Smooth scrubbing effect
-      markers: false, // For debugging, remove in production
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      scrub: true,
+      markers: false,
     },
   });
 };
@@ -60,10 +60,10 @@ const animateSpacer = (spacer) => {
       duration: 1,
       scrollTrigger: {
         trigger: spacer,
-        start: "top bottom", // Start animation when spacer top hits the bottom of the viewport
-        end: "bottom top", // End animation when spacer bottom leaves the top of the viewport
+        start: "top bottom",
+        end: "bottom top",
         scrub: true,
-        markers: false, // For debugging, remove in production
+        markers: false,
       },
     }
   );
@@ -72,17 +72,17 @@ const animateSpacer = (spacer) => {
 const animateVariants = () => {
   gsap.utils.toArray(".variants .variant").forEach((variant, i) => {
     gsap.from(variant, {
-      x: i % 2 === 0 ? -300 : 300, // Alternating direction
+      x: i % 2 === 0 ? -300 : 300,
       rotation: i % 2 === 0 ? -90 : 90,
       opacity: 0,
       duration: 0.5,
       ease: "power2.out",
       scrollTrigger: {
         trigger: variant,
-        start: "top center", // Start when the top of the variant is in the center of the viewport
-        end: "bottom top", // End when the bottom of the variant is at the top of the viewport
+        start: "top center",
+        end: "bottom top",
         toggleActions: "play none none reset",
-        markers: false, // For debugging, remove in production
+        markers: false,
       },
     });
   });
@@ -92,7 +92,6 @@ const animateHistory = () => {
   let historyPanels = gsap.utils.toArray(".history-container .history-article");
   let historyContainer = document.querySelector(".history-container");
 
-  // Set up horizontal scrolling for the history section
   gsap.to(historyPanels, {
     xPercent: -100 * (historyPanels.length - 1),
     ease: "none",
@@ -103,7 +102,7 @@ const animateHistory = () => {
       end: () =>
         "+=" + historyContainer.offsetWidth * (historyPanels.length - 1),
       scrub: true,
-      markers: true, // For debugging, remove in production
+      markers: true,
     },
   });
 };
